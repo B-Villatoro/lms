8565bdffbfa33e56a27a6add8c7f7be5b2c857c7
Bryan Villatoro <bvillatoro@ymail.com>
2019-08-23 15:46:17 -0400

    eureka servers connected

--- src/main/java/com/team/alpha/lms_orchestrator/LmsOrchestratorApplication.java
+++ src/main/java/com/team/alpha/lms_orchestrator/LmsOrchestratorApplication.java
@@ -2,10 +2,21 @@ package com.team.alpha.lms_orchestrator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LmsOrchestratorApplication {
 
+	@LoadBalanced 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LmsOrchestratorApplication.class, args);
 	}
--- src/main/java/com/team/alpha/lms_orchestrator/rest_client/BorrowerRestClient.java
+++ src/main/java/com/team/alpha/lms_orchestrator/rest_client/BorrowerRestClient.java
@@ -1,6 +1,10 @@
 package com.team.alpha.lms_orchestrator.rest_client;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -15,61 +19,61 @@ import com.team.alpha.lms_orchestrator.entity.BookLoan;
 @Component
 public class BorrowerRestClient implements ObjectRestClient<BookLoan> {
 
-	private static final String BOOKLOAN_REST_URI = "http://localhost:8081/api/borrower/";
-	
-	
+	private static final String BOOKLOAN_REST_URI = "http://borrower/api/borrower/";
+
+	@Autowired
+	RestTemplate restTemplate;
+
 	@Override
 	public ResponseEntity<List<BookLoan>> getAllObjects() {
-		String BOOKLOAN_REST_URL = BOOKLOAN_REST_URI+"/bookloans";
-		RestTemplate restTemplate = new RestTemplate();
+		String BOOKLOAN_REST_URL = BOOKLOAN_REST_URI + "bookloans";
 		ParameterizedTypeReference<List<BookLoan>> responseType = new ParameterizedTypeReference<List<BookLoan>>() {
 		};
 		ResponseEntity<List<BookLoan>> response = restTemplate.exchange(BOOKLOAN_REST_URL, HttpMethod.GET, makeEntity(),
 				responseType);
 		return response;
 	}
-	
-	
+
 	public ResponseEntity<List<BookLoan>> getAllObjectsById(int cardNumber) {
-		String BOOKLOAN_REST_URL = BOOKLOAN_REST_URI + cardNumber + "/bookloans";
-		RestTemplate restTemplate = new RestTemplate();
+		String BOOKLOAN_REST_URL = BOOKLOAN_REST_URI +cardNumber+ "/bookloans";
 		ParameterizedTypeReference<List<BookLoan>> responseType = new ParameterizedTypeReference<List<BookLoan>>() {
 		};
 		ResponseEntity<List<BookLoan>> response = restTemplate.exchange(BOOKLOAN_REST_URL, HttpMethod.GET, makeEntity(),
 				responseType);
 		return response;
 	}
+
 	@Override
 	public ResponseEntity<BookLoan> createObject(BookLoan bookLoan) {
-		String BOOKLOAN_REST_URL = BOOKLOAN_REST_URI + bookLoan.getCardNo() + "/branch/" + bookLoan.getBranchId()
-				+ "/bookid/" + bookLoan.getBookId();
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<BookLoan> response = restTemplate.exchange(BOOKLOAN_REST_URL, HttpMethod.POST, makeEntity(),
-				BookLoan.class);
+		String BOOKLOAN_REST_URL = BOOKLOAN_REST_URI + "/bookloan/checkout";
+		ResponseEntity<BookLoan> response = restTemplate.postForEntity(BOOKLOAN_REST_URL, bookLoan, BookLoan.class);
 		return response;
 	}
+
 	@Override
-	public ResponseEntity<BookLoan> deleteObject(BookLoan bookLoan) {
-		String BOOKLOAN_REST_URL = BOOKLOAN_REST_URI + bookLoan.getCardNo() + "/branch/" + bookLoan.getBranchId()
-				+ "/bookid/" + bookLoan.getBookId();
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<BookLoan> response = restTemplate.exchange(BOOKLOAN_REST_URL, HttpMethod.DELETE, makeEntity(),
-				BookLoan.class);
-		return response;
+	public void deleteObject(int bookId,int branchId,int cardNumber) {
+		String BOOKLOAN_REST_URL = BOOKLOAN_REST_URI + "/bookloan/returnbook/book/{bookId}/branch/{branchId}/cardno/{cardNumber}";
+		
+		Map<String,String> variables = new HashMap<String, String>();
+		variables.put("bookId",""+bookId);
+		variables.put("branchId",""+branchId);
+		variables.put("cardNumber",""+cardNumber);
+		
+		restTemplate.delete(BOOKLOAN_REST_URL,variables);
 	}
+
 	@Override
 	public ResponseEntity<BookLoan> updateObject(BookLoan bookLoan) {
 		String BOOKLOAN_REST_URL = BOOKLOAN_REST_URI + bookLoan.getCardNo() + "/branch/" + bookLoan.getBranchId()
 				+ "/bookid/" + bookLoan.getBookId();
-		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<BookLoan> response = restTemplate.exchange(BOOKLOAN_REST_URL, HttpMethod.PUT, makeEntity(),
 				BookLoan.class);
 		return response;
 	}
-	
+
 	private HttpEntity<?> makeEntity() {
 		HttpHeaders header = new HttpHeaders();
 		header.setContentType(MediaType.APPLICATION_JSON);
-		return new HttpEntity<>(header);	
+		return new HttpEntity<>(header);
 	}
 }
